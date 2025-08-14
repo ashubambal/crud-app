c7c19331b474be1b21a299d08b7001ef023d1132
Ubuntu <ubuntu@ip-172-31-42-14.ap-southeast-1.compute.internal>
2025-08-14 20:02:14 +0000

    dummy

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const port = 3000;
 
+
 // MySQL connection
 const db = mysql.createConnection({
     host: 'testdb-1.cp24ccc4chcf.ap-southeast-1.rds.amazonaws.com',
