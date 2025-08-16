2f7624738ad5233c412ee4a39d420258770bd253
Ubuntu <ubuntu@ip-172-31-32-105.ap-southeast-1.compute.internal>
2025-08-16 05:31:43 +0000

    Adding Gihub Workflow

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 
+
 const app = express();
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
